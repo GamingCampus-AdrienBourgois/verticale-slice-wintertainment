c14b94156be54a15be5ef31a26adb47717b63d32
unknown <jmontes@gamingcampus.local>
2023-12-15 10:59:23 +0100

    Musique en commentaire

--- SFMLEngine/include/Components/ViewComponent.cpp
+++ SFMLEngine/include/Components/ViewComponent.cpp
@@ -12,6 +12,22 @@ void ViewComponent::Render(sf::RenderWindow* _window)
 {
 	view.setSize(240, 135);
 	Camera = GetOwner()->GetPosition();
+	if (Camera.x < 120)
+	{
+		Camera.x = 120;
+	}
+	else if (Camera.x > 360 )
+	{
+		Camera.x = 360;
+	}
+	if (Camera.y < 68)
+	{
+		Camera.y = 68;
+	}
+	else if (Camera.y > 252)
+	{
+		Camera.y = 252;
+	}
 	view.setCenter(Camera.x, Camera.y);
 	_window->setView(view);
 }
